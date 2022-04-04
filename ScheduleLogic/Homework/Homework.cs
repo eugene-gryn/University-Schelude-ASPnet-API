ed4d0fa8c79b20def510abb403bc7427df89f248
Dmitriy Yaremenko <dmitriy@zala.com.a>
2022-04-04 14:41:20 +0300

    Homework added

---
+++ ScheduleLogic/Homework/Homework.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleLogic.Homework
+{
+    public class HomeWork
+    {
+        public string LessonName { get; set; }
+        public string HomeMade { get; set; }
+        public bool IsCompleted { get; set; } = false;
+        public byte HomeWorkPriority { get; set; }
+        DateTime time = new DateTime();
+
+        public HomeWork()
+        {
+
+        }
+        public HomeWork(string lessonName, string homeMade)
+        {
+            LessonName = lessonName;
+            HomeMade = homeMade;
+        }
+
+    }
+}
---
+++ ScheduleLogic/Homework/HomeworkManagment.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleLogic.Homework
+{
+    public class HomeWorkManagment
+    {
+
+        List<HomeWork> homeWorks;
+
+        HomeWork homeWork = new HomeWork();
+        //для сериалізації
+        public HomeWorkManagment()
+        {
+
+        }
+        //для створення домашки
+        public HomeWorkManagment(DateTime deadline, byte HomeWorkPriority = 5)
+        {
+            Deadline = deadline;
+            this.HomeWorkPriority = HomeWorkPriority;
+        }
+        //метод сортировки після визначення пріорітетності
+        private void SortHomeWork(List<HomeWork> homeWorks)
+        {
+            var sorted = homeWorks.OrderBy(hw => hw.HomeWorkPriority);
+            foreach (var hw in sorted)
+                Console.WriteLine(hw);
+        }
+        public void ChangeHomeWork(List<HomeWork> homeWorks, int index, string homeMade)
+        {
+            homeWorks[index].HomeMade = homeMade;
+        }
+        public void DeleteHomeWork(List<HomeWork> homeWorks, int index)
+        {
+            homeWorks[index].Remove();
+        }
+        public void ChangeDeadLine(List<HomeWork> homeWorks, int index, DateTime date)
+        {
+            homeWorks[index].time = date;
+        }
+        public bool IsReadyHW()
+        {
+            if (homeWork.IsCompleted)
+            {
+                return true;
+            }
+            return homeWork.IsCompleted;
+        }
+    }
+}
