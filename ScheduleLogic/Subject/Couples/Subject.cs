c08861d4d69db131ede705c685ef2d60a80898fc
pinirus <zuvado11@yandex.ua>
2022-04-05 13:56:29 +0300

    One similar type

--- ScheduleLogic/Subject/Couples/Subject.cs
+++ ScheduleLogic/Subject/Couples/Subject.cs
@@ -1,6 +1,8 @@
-﻿namespace ScheduleLogic.Subject.Couples
+﻿using System.Collections.Generic;
+
+namespace ScheduleLogic.Subject.Couples
 {
-    public class Subject
+    public class Subject : IEqualityComparer<Subject>
     {
         public string Name;
         public string GoogleMeetUrl;
@@ -11,6 +13,23 @@
             Name = name;
             GoogleMeetUrl = googleMeetUrl;
             this.isPractice = isPractice;
+        }
+
+        public bool Equals(Subject x, Subject y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            if (x.GetType() != y.GetType()) return false;
+            return x.Name == y.Name && x.isPractice == y.isPractice;
+        }
+
+        public int GetHashCode(Subject obj)
+        {
+            unchecked
+            {
+                return ((obj.Name != null ? obj.Name.GetHashCode() : 0) * 397) ^ obj.isPractice.GetHashCode();
+            }
         }
     }
 }
---
+++ ScheduleLogic/Subject/Couples/SubjectSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleLogic.Subject.Couples
+{
+    public static class SubjectSource
+    {
+        public static HashSet<Subject> Subjects { get; set; }
+
+        static SubjectSource()
+        {
+            Subjects = new HashSet<Subject>();
+        }
+
+        public static Subject GetSubject(string name, bool isPractice)
+        {
+            if (Subjects.Contains(new Subject(name, isPractice)))
+            {
+
+            }
+
+            return null;
+        }
+    }
+}
