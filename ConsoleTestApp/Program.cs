7ac9da4e61d81181eff5b8abc4ec1101e0f88f46
Grin <zuvado11@yandex.ua>
2022-06-22 12:50:57 +0300

    Fix project

--- ConsoleTestApp/Program.cs
+++ ConsoleTestApp/Program.cs
@@ -7,5 +7,33 @@ User user = new User()
     Name = "Kuk",
     ImageLocation = "kukokkkk",
     Password = new byte[] {0x12, 0x14, 0xFF},
-    Salt = new byte[] {0x14, 0x10, 0x00}
+    Salt = new byte[] {0x14, 0x10, 0x00},
+    Settings =
+    {
+        NotifyAboutCouple = true,
+        NotifyBeforeCouple = true,
+        NotifyAboutHomework = true,
+        NotifyAboutDeadlineHomework = true,
+        NotifyAboutLoseDeadlineHomework = true
+    },
+
+    Groups = new List<Group>()
+    {
+        new Group() { 
+            Id = 1,
+            Name = "ddd", 
+            Users = new List<User>()
+            {
+
+            },
+
+
+        }
+    },
+
+    Homeworks = new List<Homework>()
+    {
+    },
+
+    IsAdmin = true
 };
