7a9525e08301cd220be276d6523d7153121287bd
Grin <zuvado11@yandex.ua>
2022-06-22 12:38:46 +0300

    Project changes

---
+++ ConsoleTestApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using DAL.Entities;
+
+User user = new User()
+{
+    Id = 0,
+    Login = "123123",
+    Name = "Kuk",
+    ImageLocation = "kukokkkk",
+    Password = new byte[] {0x12, 0x14, 0xFF},
+    Salt = new byte[] {0x14, 0x10, 0x00}
+};
