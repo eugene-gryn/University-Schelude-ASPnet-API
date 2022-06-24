a8649e1f40305ec7f0ae9d46c30f94c03722518b
Grin <zuvado11@yandex.ua>
2022-06-24 11:10:43 +0300

    Repositories

--- DAL/Repository/CouplesRepository.cs
+++ DAL/Repository/CouplesRepository.cs
@@ -1,37 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DAL.EF;
+﻿using DAL.EF;
 using DAL.Entities;
+using Microsoft.EntityFrameworkCore;
 
-namespace DAL.Repository
+namespace DAL.Repository;
+
+public class CouplesRepository : EFRepository<Couple>
 {
-    public class CouplesRepository : EFRepository<Couple>
+    public CouplesRepository(ScheduleContext context) : base(context)
     {
-        public CouplesRepository(ScheduleContext context) : base(context)
+    }
+
+    public override async Task<Couple> Create(Couple item)
+    {
+        item.Id = 0;
+
+        await Context.Couples.AddAsync(item);
+    }
+
+    public override IQueryable<Couple> Read()
+    {
+        return Context.Couples.AsQueryable();
+    }
+
+    public override Task<bool> Update(Couple item)
+    {
+        Context.Entry(item).State = EntityState.Modified;
+
+        return Task.FromResult(true);
+    }
+
+    public override async Task<bool> Delete(int id)
+    {
+        var couple = await Context.Couples.Where(coupleT => coupleT.Id == id).FirstOrDefaultAsync();
+
+        if (couple != null)
         {
+            Context.Remove(couple);
+            return true;
         }
 
-        public override Task<Couple> Create(Couple item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override IQueryable<Couple> Read()
-        {
-            return Context.Couples.AsQueryable();
-        }
-
-        public override Task<bool> Update(Couple item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override Task<bool> Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
+        return false;
     }
 }
